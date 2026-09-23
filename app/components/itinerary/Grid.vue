d3bf3d0ce4617b1725dd2ca240b5441f9d768c0a
Regi Diocera <rmdiocera@gmail.com>
2026-09-23 16:41:59 +0800

    refactor: disable editing details of past itineraries

--- app/components/itinerary/Grid.vue
+++ app/components/itinerary/Grid.vue
@@ -33,6 +33,7 @@ const items = (itinerary: Itinerary) => [
       emit('isEditing', true)
       emit('selectedItinerary', itinerary)
     },
+    disabled: toCalendarDate(new Date(itinerary.start_date)) < toCalendarDate(new Date())
   },
   {
     label: 'Delete',
